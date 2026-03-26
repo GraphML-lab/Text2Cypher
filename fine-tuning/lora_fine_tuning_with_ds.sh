a5aebf4a503ca232c20cde246d9e4d8f55742f08
Yacklin Wong <139425274+Yacklin@users.noreply.github.com>
2026-03-26 11:17:42 -0300

    Delete fine-tuning directory

--- fine-tuning/lora_fine_tuning_with_ds.sh
+++
@@ -1,2 +0,0 @@
-export LD_LIBRARY_PATH=/data/home/yilin/projects/fine-tuning/.venv/lib64/python3.11/site-packages/nvidia/nvjitlink/lib:$LD_LIBRARY_PATH
-deepspeed lora_fine_tuning_with_ds.py
--- fine-tuning/merge_peft.sh
+++
@@ -1,2 +0,0 @@
-export LD_LIBRARY_PATH=/data/home/yilin/projects/fine-tuning/.venv/lib64/python3.11/site-packages/nvidia/nvjitlink/lib:$LD_LIBRARY_PATH
-python3 merge_peft.py
